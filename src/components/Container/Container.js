c1799a987b7c0afce80357a4e76ee63cf4414d2a
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-09-29 01:29:14 +0600

    json create and fiavicon icon add

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import'./Container.css';
 
 const Container = () => {
     return (
         <div>
             <h4>Select Your Exercise</h4>
+            <div className="all-container">
+                <div className="left-container">
+                    <p>This is Left Container</p>
+                </div>
+                <div className="right-container">
+                    <p>This is right Container</p>
+                </div>
+            </div>
         </div>
     );
 };
