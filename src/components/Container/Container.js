5e81a277e234a7f02c855456ba9cb49abd736ce0
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-09-28 23:27:20 +0600

    End Of Header

---
+++ src/components/Container/Container.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Container = () => {
+    return (
+        <div>
+            <h4>Select Your Exercise</h4>
+        </div>
+    );
+};
+
+export default Container;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeadset } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+    return (
+        <div>
+            <h2><FontAwesomeIcon icon={faHeadset} />  Best Health Club and Gyms</h2>
+             
+        </div>
+    );
+};
+
+export default Header;
